9e1b8a97bdaf2468d46010dc1894d64ccdf0ea68
Ozzy Yang <freefantasystation@gmail.com>
2022-06-02 18:10:38 +0800

    优化日志记录

--- app.js
+++ app.js
@@ -1,62 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const config = require("./store/config");
+const dayjs = require("dayjs");
 
 //配置跨域访问的全局中间件
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 //配置解析表单数据的全局中间件
 app.use(express.urlencoded({ extended: false }));
 
 //在3007接口进行监听
-app.listen(3007, () => {
-    console.log('Api Server on:3007');
-})
+app.listen(config.port, () => {
+  console.log("Api Server on:" + config.port);
+});
 
 //配置解析Token的中间件
-const { expressjwt: expressJwt } = require('express-jwt');
-const config = require('./store/config');
-app.use(expressJwt({ secret: config.jwtSecretKey, algorithms: ["HS256"] }).unless({ path: [/^\/user\//] }));
+const { expressjwt: expressJwt } = require("express-jwt");
+
+app.use(
+  expressJwt({ secret: config.jwtSecretKey, algorithms: ["HS256"] }).unless({
+    path: [/^\/user\//]
+  })
+);
 
 //全局的响应函数的中间件
 app.use((req, res, next) => {
-    res.cc = (msg, status = 1, data = undefined) => {
-        //如果msg是一种错误，则取值为错误信息
-        msg = msg instanceof Error ? msg.message : msg;
-        console.log(status === 0 ? 'success' : msg);
-        res.send({
-            status,
-            message: msg,
-            data: data
-        })
-    }
-    next();
-})
+  /**
+   * @param {发送给前端的错误信息} msg
+   * @param {输出在控制面板的日志信息} logInfo
+   * @param {发送给前端的执行状态,0表示成功,1表示失败} status
+   * @param {发送给前端的数据} data
+   */
+  res.cc = (msg, logInfo, status = 0, data = undefined) => {
+    //如果msg是一种错误，则取值为错误信息
+    msg = msg instanceof Error ? msg.message : msg;
+    console.log(
+      dayjs().format("YYYY-MM-DD HH:mm:ss"),
+      ":",
+      logInfo,
+      status === 1 ? "success" : msg
+    );
+    res.send({
+      status,
+      message: msg,
+      data: data
+    });
+  };
+  next();
+});
 
 //导入用户路由模块
-const userRouter = require('./router/user.js');
-app.use('/user', userRouter);
+const userRouter = require("./router/user.js");
+app.use("/user", userRouter);
 
 //导入个人中心路由模块
-const myRouter = require('./router/my.js');
-app.use('/my', myRouter);
+const myRouter = require("./router/my.js");
+app.use("/my", myRouter);
 
 //捕获Token认证后的错误
 app.use((err, req, res, next) => {
-    console.log(err.message);
-    // token解析失败导致的错误
-    if (err.name === 'UnauthorizedError') {
-        //TODO 无效的cc function
-        return res.send({
-            status: 1,
-            msg: '无效的Token'
-        })
-    }
-    //其它原因导致的错误
+  console.log(err.message);
+  // token解析失败导致的错误
+  if (err.name === "UnauthorizedError") {
+    //TODO 无效的cc function
     return res.send({
-        status: 1,
-        msg: 'Token认证失败'
-    })
-})
-
-
+      status: 1,
+      msg: "无效的Token"
+    });
+  }
+  //其它原因导致的错误
+  return res.send({
+    status: 1,
+    msg: "Token认证失败"
+  });
+});
--- router_handler/user_handler.js
+++ router_handler/user_handler.js
@@ -1,86 +1,91 @@
-const database = require('../database/index');
+const database = require("../database/index");
 //用于加密用户密码的中间件
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 //引入全局共享的变量和方法
-const config = require('../store/config');
+const config = require("../store/config");
 //配置Token的中间件
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 //用于验证用户信息的中间件
-const { validationResult } = require('express-validator');
-
+const { validationResult } = require("express-validator");
 
 //添加新的用户
 exports.addUser = (req, res) => {
-    //获取用户信息
-    var userinfo = req.body;
-    console.log(`add user [${userinfo.username}]`);
+  //获取用户信息
+  var userinfo = req.body;
+  const logInfo = `add user [${userinfo.username}]`;
 
-    //验证用户信息是否为合法的格式
-    const checkErrors = validationResult(req);
-    if (checkErrors.array().length > 0) {
-        return res.cc(checkErrors.array(true)[0].msg);
+  //验证用户信息是否为合法的格式
+  const checkErrors = validationResult(req);
+  if (checkErrors.array().length > 0) {
+    return res.cc(checkErrors.array(true)[0].msg, logInfo);
+  }
+
+  const sqlSel = "select * from wms_user where username=?";
+  database.query(sqlSel, [userinfo.username], (err, results) => {
+    //执行语句失败
+    if (err) {
+      return res.cc(err);
+    }
+    //用户名被占用
+    if (results.length > 0) {
+      return res.cc("用户名被占用，请选择其它用户名", logInfo);
     }
 
-    const sqlSel = 'select * from wms_user where username=?';
-    database.query(sqlSel, [userinfo.username], (err, results) => {
-        //执行语句失败
-        if (err) {
-            return res.cc(err);
-        }
-        //用户名被占用
-        if (results.length > 0) {
-            return res.cc('用户名被占用，请选择其它用户名');
-        }
-
-        //调用bcrypt.hashSync()对密码进行加密
-        userinfo.password = bcrypt.hashSync(userinfo.password, 10);
-        //插入新用户
-        const sqlIns = 'insert into wms_user set ?'
-        database.query(sqlIns, userinfo, (err, results) => {
-            if (err) {
-                return res.cc(err)
-            }
-            if (results.affectedRows !== 1) {
-                return res.cc('用户注册失败，请稍后再试')
-            }
-            return res.cc('注册成功', 0)
-        })
-    })
-}
+    //调用bcrypt.hashSync()对密码进行加密
+    userinfo.password = bcrypt.hashSync(userinfo.password, 10);
+    //插入新用户
+    const sqlIns = "insert into wms_user set ?";
+    database.query(sqlIns, userinfo, (err, results) => {
+      if (err) {
+        return res.cc(err, logInfo);
+      }
+      if (results.affectedRows !== 1) {
+        return res.cc("用户注册失败，请稍后再试", logInfo);
+      }
+      return res.cc("注册成功", logInfo, 1);
+    });
+  });
+};
 
 //用户进行登陆
 exports.userLogin = (req, res) => {
-    //获取用户信息
-    const userinfo = req.body;
-    console.log(`user login [${userinfo.username}]`);
+  //获取用户信息
+  const userinfo = req.body;
+  const logInfo = `user login [${userinfo.username}]`;
 
-    //验证用户信息是否为合法的格式
-    const errors = validationResult(req);
-    if (errors.array().length > 0) {
-        return res.cc(errors.array(true)[0].msg);
+  //验证用户信息是否为合法的格式
+  const errors = validationResult(req);
+  if (errors.array().length > 0) {
+    return res.cc(errors.array(true)[0].msg, logInfo);
+  }
+
+  const sqlSel = "select * from wms_user where username=?";
+  database.query(sqlSel, [userinfo.username], (err, results) => {
+    //执行语句失败
+    if (err) {
+      return res.cc(err, logInfo);
+    }
+    //查询到的数据不为1，则登陆失败
+    if (results.length !== 1) {
+      return res.cc("登陆失败，用户未找到", logInfo);
     }
 
-    const sqlSel = 'select * from wms_user where username=?';
-    database.query(sqlSel, [userinfo.username], (err, results) => {
-        //执行语句失败
-        if (err) {
-            return res.cc(err);
-        }
-        //查询到的数据不为1，则登陆失败
-        if (results.length !== 1) {
-            return res.cc('登陆失败，用户未找到');
-        }
+    //判断用户输入密码是否正确
+    const compareResult = bcrypt.compareSync(
+      userinfo.password,
+      results[0].password
+    );
+    if (!compareResult) {
+      return res.cc("登陆失败", logInfo);
+    }
 
-        //判断用户输入密码是否正确
-        const compareResult = bcrypt.compareSync(userinfo.password, results[0].password);
-        if (!compareResult) {
-            return res.cc('登陆失败')
-        }
-
-        //清洗用户信息并生成Token
-        const userInfo = { ...results[0], password: '', user_pic: '' };
-        const tokenStr = jwt.sign(userInfo, config.jwtSecretKey, { expiresIn: config.tokenDuration });
-        return res.cc('登陆成功', 0, { token: tokenStr })
-    })
-
-}
+    //清洗用户信息并生成Token
+    const userInfo = { ...results[0], password: "", user_pic: "" };
+    const tokenStr = jwt.sign(userInfo, config.jwtSecretKey, {
+      expiresIn: config.tokenDuration
+    });
+    return res.cc("登陆成功", logInfo, 1, {
+      token: tokenStr
+    });
+  });
+};
